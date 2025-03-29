36d56d492a232a2be6746facfd9ac65e0f73efcc
mamontuka <mamontuka@gmail.com>
2025-03-29 15:21:12 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,9 @@ set -e
 echo "Ritar BMS Service started"
 
 ## start ritar-bms web service
-python3 -m http.server 50501 -d /web_ui &
+python3 -m http.server 50501 -d /web_ui 2> /dev/null &
 
 ## start ritar-bms main part
 while true; do
 python3 -u ../ritar-bms.py
 done
-
-
