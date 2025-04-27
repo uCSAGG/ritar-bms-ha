982e77f9e1a823b3dd002afaeaba98a5db92004d
mamontuka <mamontuka@gmail.com>
2025-04-27 22:58:39 +0300

    Create protocol.py

---
+++ protocol.py
@@ -0,0 +1,36 @@
+# Battery 1 Queries
+bat_1_get_block_voltage = b'\x01\x03\x00\x00\x00\x10\x44\x06'  # Battery voltage query for Battery 1
+bat_1_not_decrypted_1 = b'\x01\x03\x00\x21\x00\x01\xd4\x00'  # Unknown at now
+bat_1_get_cells_voltage = b'\x01\x03\x00\x28\x00\x10\xc4\x0e'  # Cells voltage query for Battery 1
+bat_1_get_temperature = b'\x01\x03\x00\x78\x00\x04\xc4\x10'  # Temperature query for Battery 1
+bat_1_not_decrypted_2 = b'\x01\x03\x00\x9b\x00\x02\xb5\xe4'  # Unknown at now
+bat_1_get_extra_temperature = b'\x01\x03\x00\x91\x00\x0a\x94\x20'  # Extra temperature query for Battery 1
+bat_1_not_decrypted_3 = b'\x01\x03\x00\xef\x00\x06\xf4\x3d'  # Unknown at now
+
+# Battery 2 Queries
+bat_2_get_block_voltage = b'\x02\x03\x00\x00\x00\x10\x44\x35'  # Battery voltage query for Battery 2
+bat_2_not_decrypted_1 = b'\x02\x03\x00\x21\x00\x01\xd4\x33'  # Unknown at now
+bat_2_get_cells_voltage = b'\x02\x03\x00\x28\x00\x10\xc4\x3d'  # Cells voltage query for Battery 2
+bat_2_get_temperature = b'\x02\x03\x00\x78\x00\x04\xc4\x23'  # Temperature query for Battery 2
+bat_2_not_decrypted_2 = b'\x02\x03\x00\x9b\x00\x02\xb5\xd7'  # Unknown at now
+bat_2_get_extra_temperature = b'\x02\x03\x00\x91\x00\x0a\x94\x13'  # Extra temperature query for Battery 2
+bat_2_not_decrypted_3 = b'\x02\x03\x00\xef\x00\x06\xf4\x0e'  # Unknown at now
+
+# Battery 3 Queries
+bat_3_get_block_voltage = b'\x03\x03\x00\x00\x00\x10\x45\xe4'  # Battery voltage query for Battery 3
+bat_3_not_decrypted_1 = b'\x03\x03\x00\x21\x00\x01\xd5\xe2'  # Unknown at now
+bat_3_get_cells_voltage = b'\x03\x03\x00\x28\x00\x10\xc5\xec'  # Cells voltage query for Battery 3
+bat_3_get_temperature = b'\x03\x03\x00\x78\x00\x04\xc5\xf2'  # Temperature query for Battery 3
+bat_3_not_decrypted_2 = b'\x03\x03\x00\x9b\x00\x02\xb4\x06'  # Unknown at now
+bat_3_get_extra_temperature = b'\x03\x03\x00\x91\x00\x0a\x95\xc2'  # Extra temperature query for Battery 3
+bat_3_not_decrypted_3 = b'\x03\x03\x00\xef\x00\x06\xf5\xdf'  # Unknown at now
+
+# Battery 4 Queries
+bat_4_get_block_voltage = b'\x04\x03\x00\x00\x00\x10\x44\x53'  # Battery voltage query for Battery 4
+bat_4_not_decrypted_1 = b'\x04\x03\x00\x21\x00\x01\xd4\x55'  # Unknown at now
+bat_4_get_cells_voltage = b'\x04\x03\x00\x28\x00\x10\xc4\x5b'  # Cells voltage query for Battery 4
+bat_4_get_temperature = b'\x04\x03\x00\x78\x00\x04\xc4\x45'  # Temperature query for Battery 4
+bat_4_not_decrypted_2 = b'\x04\x03\x00\x9b\x00\x02\xb5\xb1'  # Unknown at now
+bat_4_get_extra_temperature = b'\x04\x03\x00\x91\x00\x0a\x94\x75'  # Extra temperature query for Battery 4
+bat_4_not_decrypted_3 = b'\x04\x03\x00\xef\x00\x06\xf4\x68'  # Unknown at now
+
